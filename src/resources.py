f62250e014e0e630646a82996e4235b5c4879c06
Jean Guilmette <Jean.Guilmette@gmail.com>
2014-10-21 09:39:06 -0400

    Rework relation between zone/building/ressource

--- src/resources.py
+++ src/resources.py
@@ -3,8 +3,8 @@ import zone
 #from zone import ZoneModifier
 __author__ = 'SJS'
 
-resourceDef = dict( \
-    Agriculture = dict(unitMax = 15000, delay = 31, ratio = 100, rate = 10),
+resourceDef = dict(
+    Agriculture = dict(unitMax=15000, delay=31, ratio=100, rate=10),
     Chasse  = dict(unitMax = 7000, delay = 91, ratio = 100, rate = 20),
     Peche  = dict(unitMax = 8000, delay = 91, ratio = 100, rate = 30), # il faut garder les trois sortes de source de nourriture pour la partie <<des choix pour une gestion durable>> du jeu, meme chose pour les trois sortes de mat/riaux qui tant qu'a eux ne devrait pas etre integre en une seule ressorce, ''materiax''. 
     Bois  = dict(unitMax = 10000, delay = 365, ratio = 100, rate = 40),
@@ -12,12 +12,12 @@ resourceDef = dict( \
     Pierre  = dict(unitMax = 8000, delay = -31000, ratio = 100, rate = 0),
     Nourriture  = dict(unitMax = 2000, delay = -310000, ratio = 100, rate = 0), #nourriture prepare pour etre mangeable pour la population (ransforme)
     Materiaux  = dict(unitMax = 2000, delay = -310000, ratio = 100, rate = 0), #il va vraiment falloir utuliser les ressouces decrite plus haut, sinon perte de l'ampleur des catastrophes
-    Energie  = dict(unitMax = 2000, delay = -310000, ratio = 100, rate = 0), #il n<y a pas vraiment de maximun d'energie prodit. Devrait etre ailleurs, avec l'argent
+    Energie  = dict(unitMax = 2000, delay = -310000, ratio = 100, rate = 0), #il n'y a pas vraiment de maximun d'energie produit. Devrait etre ailleurs, avec l'argent
     TestResA  = dict(unitMax = 1000, delay = 31, ratio = 100, rate = 1),  # Should not be modified, used to valid internal mechanic of the game 
     TestResB  = dict(unitMax = 5000, delay = 31, ratio = 100, rate = 10)  # Should not be modified, used to valid internal mechanic of the game        
     )
 
-terrainType = dict( \
+terrainType = dict(
     Plaine = dict(Agriculture = 5.0, Chasse  = 1.0, Peche  =  0, Bois = 0.75, Metaux = 0.25 , Pierre = 0.35 ),
     Foret = dict(Agriculture = 0.75, Chasse  = 2.0, Peche  =  0, Bois = 4.0, Metaux = 0.1 , Pierre = 0.1  ),
     Collines = dict(Agriculture = 0.5, Chasse  = 0.75, Peche  =  0, Bois = 0.5, Metaux = 4.0 , Pierre = 5.0 ),
@@ -27,8 +27,9 @@ terrainType = dict( \
     EauxSalees = dict(Agriculture = 0, Chasse  = 0, Peche  =  8.0, Bois = 0 , Metaux = 0 , Pierre = 0 )
     )
 
+
 # # This class identify resource type available and define its behavior in the game
-class Resource():
+class Resource(object):
 
     def __init__(self, name):
         self.name = name        # Name/ID of the resource
