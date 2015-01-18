d5f19f1c296cddef1485dd2aebb346a00cef8b3c
Jean Guilmette <Jean.Guilmette@gmail.com>
2015-01-18 18:19:17 -0500

    First implementation of Event class

--- src/Events.py
+++ src/Events.py
@@ -6,8 +6,9 @@ Created on Dec 24, 2014
 # Pour permettre <aparition des evenements
 import pygame
 import sys; sys.path.append("../lib")
+
 from pgu import gui
-from bokeh.server.models.docs import Doc
+# from bokeh.server.models.docs import Doc
 
 ##########################################################
 #
@@ -15,38 +16,32 @@ from bokeh.server.models.docs import Doc
 class Event(gui.Dialog): 
     def __init__(self, title, description, options):
         self.name = title
+        self.font = pygame.font.SysFont(None, 30)        
+        self.space = self.font.size(" ")         
         self.desc = self.CreateDescription(description)
         self.options = self.CreateOptions(options)
-     
+
+             
     def CreateDescription(self, text):
         doc = gui.Document(width=640) 
-#         space = "  "        
+     
         doc.block(align=0)
-        for word in """Cuzco's Paint v1.0 by Phil Hassey""".split(" "): 
+        for word in text.split(" "): 
             doc.add(gui.Label(word))
-#             doc.space(space)
-#         doc.br(space[1])
+            doc.space(self.space)
+        doc.br(self.space[1])
         return doc
     
     def CreateOptions(self, options): 
         docs = []
-        doc = gui.Document(width=400) 
-        doc.block(align=-1)
-        for word in """Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!""".split(" "): 
-            doc.add(gui.Label(word))
-        docs.append(("Option A", doc)) 
-       
-        doc = gui.Document(width=400) 
-        doc.block(align=-1)
-        for word in """Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!""".split(" "): 
-            doc.add(gui.Label(word))
-        docs.append(("Option B", doc))    
-        
-        doc = gui.Document(width=400) 
-        doc.block(align=-1)
-        for word in """Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild! Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!""".split(" "): 
-            doc.add(gui.Label(word))
-        docs.append(("Option C", doc))             
+        for id, desc, effects in options:
+            doc = gui.Document(width=400) 
+            doc.block(align=-1)
+            for word in desc.split(" "): 
+                doc.add(gui.Label(word))
+                doc.space(self.space)
+            doc.br(self.space[1])                
+            docs.append((id, doc, effects)) 
         return docs
     
     
@@ -60,7 +55,7 @@ class EventsMenu(gui.Dialog):
         self.gameEvent = event
         self.value = gui.Form()
         self.widgetList = []
-
+        self.bgImage = pygame.image.load("../Src/image/Nouvelle_Aube.jpg")
 
         label_heigth = 20 
         width = 640
@@ -71,38 +66,37 @@ class EventsMenu(gui.Dialog):
         c.add(gui.Label("Event: %s" % self.gameEvent.name), 10, 10)
           
         # Description
-        c.add(gui.ScrollArea(self.gameEvent.desc, width, 100, hscrollbar=False, vscrollbar=True), 0, 40)
+        c.add(gui.ScrollArea(self.gameEvent.desc, width, 100, hscrollbar=False, vscrollbar=False), 0, 40)
     
         # Option
         y = 150
-        c.add(gui.ScrollArea(self.gameEvent.options[0][1], 440, 200, hscrollbar=False,  vscrollbar=True), 200, y)
+        self.displayOption = gui.ScrollArea(self.gameEvent.options[0][1], 440, 300, hscrollbar=False,  vscrollbar=False)
+        c.add(self.displayOption, 200, y)
         
         t = gui.Table()
-        g = gui.Group(name='options',value='A')        
-        for opt, doc in self.gameEvent.options:
+        g = gui.Group(name='options',value='0')   
+        index = 0     
+        for opt, doc, effect in self.gameEvent.options:
             t.tr()
-            t.td(gui.Radio(g,opt))
+            t.td(gui.Radio(g,index))
             t.td(gui.Label(opt))
+            index += 1
         c.add(t, 10, y)
+        g.value = "0"
+        g.connect(gui.CHANGE, self.action_SelectOption, g)
+        
+        b = gui.Button("Confirmez votre choix", width=150, height=50)
+        b.connect(gui.CLICK, self.action_ConfirmChoice, g)
+        c.add(b, 5, 400)
         gui.Dialog.__init__(self,title,c)
 
-
-    def CreateTable(self):
-        t = gui.Table(witdh=400, height=400)
-#         c = gui.Container(width=640, height=400, background=(220, 220, 220))        
-
+    def action_SelectOption(self, ctl):
+        self.displayOption.widget = self.gameEvent.options[ctl.value][1]
         
-        # Title
-        t.tr()
-        t.td(gui.Label("Event: %s" % self.gameEvent.name))
-          
-        # Description
-        t.tr()
-        t.td(gui.ScrollArea(self.gameEvent.desc, 200, 100))
-    
-        # Option
-        g = gui.Group()        
-        for opt in self.gameEvent.options:
-            t.tr()
-            t.td(gui.Radio(g,value="Option A"))
-            t.td(gui.ScrollArea(self.gameEvent.options[0], 200, 100))        
+    def action_ConfirmChoice(self, ctl):
+        print(ctl.value)
+        print(self.gameEvent.options[ctl.value][2])
+        self.close()
+        
+        
+ 
--- test/test_dialog.py
+++ test/test_dialog.py
@@ -21,8 +21,27 @@ if __name__ in '__main__':
 #     t = gui.Theme("default")   
 #     t = gui.Theme("clean")
 #    top = gui.Desktop(theme=gui.Theme(['default','tools']))
-  
-    app = gui.Desktop(theme=t, width=1024, height=800)
+#     backg = gui.Background((255,255,255), "default")
+##################################
+#
+    desc = "L'infirmerie du navire se remplit de plus en plus. Manifestement, une maladie se répand au travers les passagers. Nous ignorons se qu'elle pourrait causer, néanmoins il est impératif de réagir! Des rumeurs de contaminations par la Mort Rouge commence déjà à ébranler l'équipage."
+    choix10a=("Je pense de ce fait qu'une mise en quarantaine des malades est tout à fait justifiée; ainsi la maladie ne devrait plus se répandre.")
+    effect10a = "panic=-10 poisson=-20 bonheur=20"
+    choix10b=("J'ai donc décidé que tout les passagers pouvant offrir de leurs temps pour le soins des malades se devront de le faire. La maladie sera ainsi enrayée grâce aux petits soins que prodigueront les autres passagers.")
+    effect10b = "panic=10 poisson=-200 bonheur=20"
+    choix10c=("Par conséquent, pour éviter de créer des perturbations au seins de l'équipage et nuire à son efficacité, ainsi qu'au moral de la population, à cause de rumeurs sur ce qui ne semble qu'être que des rhumes des plus banals, il faudra étouffer les rumeurs et rassuré tout le monde.")
+    effect10c = "panic=100 bonheur=-50"
+    choix10d=("Ce qui réduirait à néant toutes chances de re-bâtir notre monde! C'est inadmissible. Pour le bien du plus grand nombre et pour éviter tout risques, nous lancerons par dessus bord tout individu suspecté de maladie, discrètement, bien sûr.")
+    effect10d = "poisson=-20 bonheur=20"
+    options =[]
+    options.append(("Option A", choix10a, effect10a))
+    options.append(("Option B", choix10b, effect10b))
+    options.append(("Option C", choix10c, effect10c))
+    options.append(("Option D", choix10d, effect10d)) 
+    evA = Events.Event("Epidemie", desc, options)          
+#####################################  
+
+    app = gui.Desktop(theme=t, width=1024, height=800, background=(255,255,255))
     app.connect(gui.QUIT,app.quit,None)
     
     c = gui.Table(width=200,height=100)
@@ -32,7 +51,7 @@ if __name__ in '__main__':
  
     dialog = BuildMenu(secteurA)  
     dialog2 = TransferMenu(secteurA)
-    evA = Events.Event("Test", "test description", "option A et B")
+
     dialog3 = Events.EventsMenu(secteurA, evA)
             
     c.tr() 
@@ -51,3 +70,5 @@ if __name__ in '__main__':
     c.td(e)  
     
     app.run(c)
+
+
